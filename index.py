58e2e930d4854c08926b2a38d07badb80ec9e08b
chagaleti332 <110809710+chagaleti332@users.noreply.github.com>
2022-08-26 18:51:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World i am ram',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
